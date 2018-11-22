15c293f832423ce72ec1055729dec90177efeaa0
Mikko Rajala <mikkorajala@hotmail.com>
2018-11-22 16:53:13 +0200

    implemented users

--- index.js
+++ index.js
@@ -1,25 +1,57 @@
-const Joi = require('joi');
+
 const express = require('express');
+const { celebrate, Joi, errors } = require('celebrate');
 const app = express();
 
 app.use(express.json());
+
+app.use(celebrate({
+  body: Joi.object({
+  name: Joi.string().min(3).required()
+ }).unknown()
+}));
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
 
 const games = [
-  {id: 1, name: 'Grand Theft Auto V'},
-  {id: 2, name: 'The Legend of Zelda Breath of The Wild'},
-  {id: 3, name: 'Tekken 7'},
+  {
+    id: 1,
+    name: 'Grand Theft Auto V'
+  },
+  {
+    id: 2,
+    name: 'The Legend of Zelda: Breath of The Wild'
+  },
+  {
+    id: 3,
+    name: 'Tekken 7'
+  },
+  {
+    id: 4,
+    name: 'Super Smash Bros Ultimate'
+  },
 ];
 
-function validateGame(game) {
-  const schema = {
-    name: Joi.string().min(3).required()
-  };
+const users = [
+  {
+    id: 1,
+    name: 'Mikko Rajala',
+    rentedGame: ''
+  },
+  {
+    id: 2,
+    name: 'Konsta Saarinen',
+    rentedGame: ''
+  },
+  {
+    id: 3,
+    name: 'Jarno Mäkelä',
+    rentedGame: ''
+  },
+];
 
-  return Joi.validate(game, schema);
-}
 
 app.get('/', (req, res) => {
   res.send('Video Game Rental Store');
@@ -29,16 +61,23 @@ app.get('/api/games', (req, res) => {
   res.send(games);
 });
 
+app.get('/api/users', (req, res) => {
+  res.send(users);
+});
+
 app.get('/api/games/:id', (req, res) => {
   const game = games.find(c => c.id === parseInt(req.params.id));
   if (!game) return res.status(404).send('The game with the given ID was not found');
   res.send(game);
 });
 
-app.post('/api/games', (req, res) => {
-  const { error } = validateGame(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+app.get('/api/users/:id', (req, res) => {
+  const user = users.find(c => c.id === parseInt(req.params.id));
+  if (!user) return res.status(404).send('The user with the given ID was not found');
+  res.send(user);
+});
 
+app.post('/api/games', (req, res) => {
   const game = {
     id: games.length + 1,
     name: req.body.name
@@ -47,21 +86,36 @@ app.post('/api/games', (req, res) => {
   res.send(game);
 });
 
+app.post('/api/users', (req, res) => {
+  const user = {
+    id: users.length + 1,
+    name: req.body.name,
+    rentedGame: ''
+  };
+  users.push(user);
+  res.send(user);
+});
+
 app.put('/api/games/:id', (req, res) => {
   //Look up the game
   //If not existing, return 404
   const game = games.find(c => c.id === parseInt(req.params.id));
   if (!game) return res.status(404).send('The game with the given ID was not found');
-
-  //validate
-  //If invalid return 400 - Bad request
-  const { error } = validateGame(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-
   //update game
   game.name = req.body.name;
   //return the updated game
   res.send(game);
+});
+
+app.put('/api/users/:id', (req, res) => {
+
+  const user = users.find(c => c.id === parseInt(req.params.id));
+  if (!user) return res.status(404).send('The user with the given ID was not found');
+
+  user.name = req.body.name;
+  user.rentedGame = req.body.rentedGame;
+
+  res.send(user);
 });
 
 app.delete('/api/games/:id', (req, res) => {
@@ -76,4 +130,15 @@ app.delete('/api/games/:id', (req, res) => {
 
   // Return the same game
   res.send(game);
+});
+
+app.delete('/api/users/:id', (req, res) => {
+
+  const user = users.find(c => c.id === parseInt(req.params.id));
+  if (!user) return res.status(404).send('The user with the given ID was not found');
+
+  const index = users.indexOf(user);
+  users.splice(index, 1);
+
+  res.send(user);
 });
